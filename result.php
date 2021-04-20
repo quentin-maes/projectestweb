5496c538d7543aab1caf59f1d5f8d2eedcfe71c6
quentin-maes <79583426+quentin-maes@users.noreply.github.com>
2021-04-21 01:31:34 +0200

    save

---
+++ result.php
@@ -0,0 +1,27 @@
+<?php
+    $username = $_POST['user_name'];
+
+
+?>
+
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Result</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+</head>
+
+<body>
+ <h1> Nom : <?php echo $username ?>
+
+ </h1>
+
+
+
+
+</body>
+</html>
