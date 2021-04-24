37922d0ac438231a99d8f955c32291f85dc30a8a
quentin-maes <79583426+quentin-maes@users.noreply.github.com>
2021-04-24 20:30:09 +0200

    save

--- result.php
+++ result.php
@@ -1,24 +1,35 @@
 <?php
     $username = $_POST['user_name'];
-
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $birthday = $_POST['birthday'];
+    $country = $_POST['country'];
+    $gender = $_POST['gender'];
+    $checkcondition = $_POST['checkcondition'];
 
 ?>
 
 
 <!doctype html>
 
-<html lang="en">
+<html lang="fr">
 <head>
   <meta charset="utf-8">
   <title>Result</title>
-  <meta name="description" content="The HTML5 Herald">
+  <meta name="resultform" content="resultform">
   <meta name="author" content="SitePoint">
 </head>
 
 <body>
- <h1> Nom : <?php echo $username ?>
+ <h2> Nom : <?php echo $username ?>
+ <br /> Email : <?php echo $email ?>
+ <br /> Mot de passe : <?php echo $password ?>
+ <br /> Date de naissance : <?php echo $birthday ?>
+ <br /> Pays : <?php echo $country ?>
+ <br /> Genre : <?php echo $gender ?>
+ <br /> Accepte les conditions : <?php echo $checkcondition ?>
 
- </h1>
+ </h2>
 
 
 
